914888b164d1c7ca9d641fc6d3aabd7507e6cde0
aj-codess <jagyei339@gmail.com>
2025-05-05 04:42:28 -0700

    progress

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import express from "express";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import express from 'express';
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app=express();
 
 const PORT=process.env.PORT || 3000;
 
+dotenv.config();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const server=app.listen(PORT, () => {
+
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+});
