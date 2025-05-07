f996d68a50785238fd51f4c938b9a2b31fafeb24
aj-codess <jagyei339@gmail.com>
2025-05-07 04:12:58 -0700

    finished user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 import User from "./../model/userModel.js";
+import logService from "../service/logService.js";
 
 const getUser=async(req,res)=>{
     try{
@@ -20,12 +21,24 @@ const getUser=async(req,res)=>{
 
 const updateProfile=async(req,res)=>{
     try{
-
+        const {username,phone,email}=req.body;
         const userId=req.user;
-        const user=await User.findById
+        const user=await User.findOneAndUpdate(
+            {id:userId},
+            {$set:{
+                username,
+                phone,
+                email
+            }},
+            {new:true}
+        );
+
+        if(user){
+            return res.status(200).json({status:"Success",message:"User Profile Updated"});
+        }
 
     } catch(error){
-        res.status(500).json({status:"failed",message:"Internal Server Error In updating User Profile"});
+        return res.status(500).json({status:"failed",message:"Internal Server Error In updating User Profile"});
         console.log("Error updating User - ",error);
     }
 };
@@ -33,6 +46,16 @@ const updateProfile=async(req,res)=>{
 
 const deleteAccount=async(req,res)=>{
     try{
+        const userId=req.user;
+        const {password}=req.body;
+        const user=await User.findOne({id:userId});
+        if(!user){
+            return res.status(404).json({status:"failed",message:"User not Found"});
+        };
+
+        if(!(await user.matchPassword(password))){
+            return res.status(406).json({status:"failed",message:"Password Unmatched. Failed Deleting User."})
+        };
 
     } catch(error){
         res.status(500).json({status:"failed",message:"Internal Server Error In Deleting User Account"});
