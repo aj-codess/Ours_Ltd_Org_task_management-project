adadfa4099adf77a122f07acfab1b6cfee4d551c
aj-codess <jagyei339@gmail.com>
2025-05-09 04:41:41 -0700

    Progress

--- controller/logController.js
+++ controller/logController.js
@@ -1,11 +1,49 @@
 import User from "./../model/userModel.js";
+import Admin from "./../model/adminModel.js";
 import utility from "./../service/utility.js";
+import logServices from "../service/logService.js";
+
+const cookieOptions = {
+    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "strict",
+  };
 
 const newUser=async(req,res)=>{
     try{
         const {email,password,phone,username}=req.body;
 
-        // const newId
+        if (!email || !password) {
+            return res.status(400).json({ error: "Email and Password Required" });
+        };
+
+        const id=utility.genId();
+
+        const newUser=new User({
+            id: id,
+            email: email,
+            username: username,
+            password: logServices.passHash(password),
+            phone: phone
+        });
+
+        const user = await newUser.save();
+
+        if (user) {
+
+            const token = await logServices.signToken(user.id);
+        
+            res.cookie('token', token, cookieOptions);
+        
+                    return res.status(200).json({
+                        username: user.username,
+                        message: 'User Created Successfully',
+                    });
+        
+          } else {
+            res.status(400).json({ error: "User not created" });
+          };
+
     } catch(error){
         res.status(500).json({status:"Failed",message:"Internal Server Error"});
         console.log(
@@ -17,6 +55,22 @@ const newUser=async(req,res)=>{
 
 const oldUser=async(req,res)=>{
     try{
+
+        const { email, password } = req.body;
+
+        const userObj=await User.findOne({email});
+  
+        if(!userObj || !(await userObj.matchPassword(password))){
+  
+          return res.status(401).json({status:"Failed",isLoggedIn: false});
+  
+        };
+  
+        const token = await logServices.signToken(userObj.id);
+        
+        res.cookie("token", token, cookieOptions);
+
+        return res.status(200).json({status:"Success",isLoggedIn:true})
 
     } catch(error){
         res.status(500).json({status:"Failed",message:"Internal Server Error"});
@@ -30,6 +84,36 @@ const oldUser=async(req,res)=>{
 const newAdmin=async(req,res)=>{
     try{
 
+        const {email,password,adminName,phone}=req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ error: "Email and Password Required" });
+        }
+
+        const adminId=utility.genId();
+
+        const adminObj=new Admin({
+            id:adminId,
+            adminName:adminName,
+            email:email,
+            password:logServices.passHash(password),
+            phone:phone
+        });
+
+        if(adminObj){
+            const token = await logServices.signToken(adminObj.id);
+        
+            res.cookie('token', token, cookieOptions);
+        
+                    return res.status(200).json({
+                        username: adminObj.adminName,
+                        message: 'User Created Successfully',
+                    });
+        
+          } else {
+            res.status(400).json({status:"Failed",message: "Admin not created" });
+          };
+
     } catch(error){
         res.status(500).json({status:"Failed",message:"Internal Server Error"});
         console.log(
@@ -41,6 +125,22 @@ const newAdmin=async(req,res)=>{
 
 const oldAdmin=async(req,res)=>{
     try{
+
+        const {email,password}=req.body;
+
+        const adminObj=await Admin.findOne({email});
+
+        if(!adminObj || !(await adminObj.matchPassword(password))){
+  
+            return res.status(401).json({status:"Failed",isLoggedIn: false});
+    
+        };
+
+        const token = await logServices.signToken(adminObj.id);
+        
+        res.cookie("token", token, cookieOptions);
+
+        return res.status(401).json({status:"Success",isLoggedIn:true});
 
     } catch(error){
         res.status(500).json({status:"Failed",message:"Internal Server Error"});
